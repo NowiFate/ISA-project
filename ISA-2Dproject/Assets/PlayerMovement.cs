b790f9e9913045011e2316d567c900dbc1385ba9
NowiFate <themadkinggangrel@gmail.com>
2020-04-03 17:45:36 +0200

    Some things I tried on the first day

---
+++ ISA-2Dproject/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float extraSpeedForRunning = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * (moveSpeed + extraSpeedForRunning);
+
+        //rennen
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            extraSpeedForRunning = 3f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            extraSpeedForRunning = 0;
+        }
+    }
+}
