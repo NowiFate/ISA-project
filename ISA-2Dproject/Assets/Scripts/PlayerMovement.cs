caca359bb18466c77eea26ae2679ca540172d90a
NowiFate <themadkinggangrel@gmail.com>
2020-04-04 17:20:13 +0200

    Day2

--- ISA-2Dproject/Assets/Scripts/PlayerMovement.cs
+++ ISA-2Dproject/Assets/Scripts/PlayerMovement.cs
@@ -2,30 +2,31 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float extraSpeedForRunning = 0;
+    public float moveSpeed = 8f;
+    public float extraSpeedForRunning = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * (moveSpeed + extraSpeedForRunning);
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+
+        transform.position += movement * Time.deltaTime * moveSpeed;
 
         //rennen
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            extraSpeedForRunning = 3f;
+            moveSpeed+=extraSpeedForRunning;
         }
 
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            extraSpeedForRunning = 0;
+            moveSpeed-=extraSpeedForRunning;
         }
     }
 }
---
+++ ISA-2Dproject/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField] private Transform teleport;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            other.transform.position = teleport.position;
+        }
+    }
+}
